d6b8fc22404c69003fb997cdf37bfb4da4a05646
Alexey Elkin <alxy.elkin@gmail.com>
2024-04-28 19:46:57 -0400

    Added data parsing for raw_sleep

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-
+import { PieChart } from 'recharts';
+import Chart1 from "./Chart1";
 
 function MainForm() {
   return (
     <div class="component">
-      
+      <div id="pie-chart">
+        <Chart1 />
+      </div>
     </div>
   )
 }

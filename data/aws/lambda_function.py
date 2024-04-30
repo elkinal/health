7f2126927b6688a14cbf8cbb9968ea7e4bb79966
Alexey Elkin <alxy.elkin@gmail.com>
2024-04-30 02:57:28 -0400

    Finished AWS lambda function that seeds DynamoDB database

--- data/aws/lambda_function.py
+++ data/aws/lambda_function.py
@@ -1,33 +1,63 @@
-
-
+import json
 import pandas as pd
+import boto3
+import io
+import random
+import uuid
 
+# Initializing credentials
 s3 = boto3.client("s3")
 bucket_name = '121health-data'
-file_key = 'raw_sleep.csv'
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('sleep_data')
+
 
 def lambda_handler(event, context):
     
-    response = s3.get_object(Bucket=bucket_name, Key=file_key)
-    
-    # Extract the content of the file
-    file_content = response['Body'].read()
-    df = pd.read_csv(response['Body'])
-    
-    # Calculating epoch time
-    df['start'] = pd.to_datetime(df['start'])
-    df['end'] = pd.to_datetime(df['end'])
-    
-    # Convert datetime objects to epoch time
-    df['start_epoch'] = df['start'].astype(int) // 10**9
-    df['end_epoch'] = df['end'].astype(int) // 10**9
-    df["duration"] = df['end_epoch'] - df['start_epoch']
-    
-    # Calculating the durations of each event
-    duration_sum = df.groupby('stage')['duration'].sum()
-    print(duration_sum)
+    # Reading contents of file into dataframes
+    raw_content = s3.get_object(Bucket=bucket_name, Key="raw_sleep.csv")['Body'].read()
+    metrics_content = s3.get_object(Bucket=bucket_name, Key="sleep_metrics.csv")['Body'].read()
 
+    raw = pd.read_csv(io.BytesIO(raw_content)).dropna()
+    metrics = pd.read_csv(io.BytesIO(metrics_content)).dropna()
+    
+    # Prevents lambda function memory exception
+    del raw_content
+    del metrics_content
+    
+    # Merging and cleaning
+    merged = pd.merge(raw, metrics, on='sleep_session_id', how='inner')
+    merged = merged.drop(['id', 'timezone', 'log_method', 'user_id', 'end', 'stage'], axis=1)
+    merged['start'] = pd.to_datetime(merged['start'])
+    merged['start'] = merged['start'].dt.date
+    merged = merged.dropna().drop_duplicates()
+    
+    merged['start'] = pd.to_datetime(merged['start'])
+    merged['start'] = merged['start'].dt.date
+    merged = merged.dropna().drop_duplicates()
+    
+    # Pivoting to relevant columns
+    df = merged.pivot_table(index='start', columns='sleep_metric', values='value', aggfunc='sum').reset_index().dropna()
+    df['start'] = df['start'].astype(str)
+    
+    print(df.dtypes)
+
+    
+    # Writing dataframe to DynamoDB
+    for _, row in df.iterrows():
+        # Convert every item in the row to a string
+        for col in row.index:
+            row[col] = str(row[col])
+        
+        item = row.to_dict()
+        table.put_item(Item=item)
+    
+        
     return {
         'statusCode': 200,
         'body': json.dumps('Success')
     }
+
+
+

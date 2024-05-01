02caca1147a9bfb0285ebb64609e5eeac5eddbc5
Alexey Elkin <alxy.elkin@gmail.com>
2024-05-01 19:56:05 -0400

    Removed credentials file

--- src/credentials.js
+++
@@ -1,8 +0,0 @@
-const config = {
-   region: 'us-east-2',
-   accessKeyId: 'AKIA6ODU4QAKGWPVH566',
-   secretAccessKey: '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',
-   openAIKey : 'Bearer sk-proj-cYqjq1fbfgFisxzS41voT3BlbkFJXOQsvFMRiYSwZu5OzMsK'
-};
- 
- module.exports = config;
